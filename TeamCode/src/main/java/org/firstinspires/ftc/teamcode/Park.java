e353bb01d537c90bdd3023d8d921c1c91251794d
kaidriscoll <kaidriscoll2020@gmail.com>
2020-01-06 16:31:38 -0700

    Park Plan

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Park.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import backcountry.Chassis;
+
+class Park {
+    private Chassis c;
+    public Park(Chassis c){
+        this.c = c;
+    }
+    public void run(){
+
+    }
+}
