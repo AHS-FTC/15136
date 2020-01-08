b3228df8618421c8a44b207e26ba35e96b6b74d5
Alex McAniff <alexmcaniff@gmail.com>
2020-01-08 12:19:38 -0700

    first TelleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelleOp.java
@@ -12,8 +12,10 @@ public class TelleOp extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private DcMotor RightFront = null;
+    private DcMotor LeftFront = null;
+    private DcMotor RightBack = null;
+    private DcMotor LeftBack = null;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -25,8 +27,10 @@ public class TelleOp extends OpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        RightFront  = hardwareMap.get(DcMotor.class, "RightFront");
+        LeftFront = hardwareMap.get(DcMotor.class, "LeftFront");
+        RightBack  = hardwareMap.get(DcMotor.class, "RightBack");
+        LeftBack = hardwareMap.get(DcMotor.class, "LeftBack");
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -44,19 +48,36 @@ public class TelleOp extends OpMode
     @Override
     public void loop() {
 
-        double leftPower;
-        double rightPower;
+        double RightBackPower;
+        double LeftBackPower;
+        double RightFrontPower;
+        double LeftFrontPower;
 
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        double strafe = -gamepad1.right_stick_y;
+        double drive  =  gamepad1.right_stick_x;
+        double turn = gamepad1.left_stick_x;
+        RightBackPower    = Range.clip(drive, -1.0, 1.0) ;
+        LeftBackPower   = Range.clip(drive, -1.0, 1.0) ;
+        RightFrontPower    = Range.clip(drive, -1.0, 1.0) ;
+        LeftFrontPower   = Range.clip(drive, -1.0, 1.0) ;
+
+        RightBackPower    = Range.clip(strafe, 1.0, -1.0) ;
+        LeftBackPower   = Range.clip(strafe, -1.0, 1.0) ;
+        RightFrontPower    = Range.clip(strafe, 1.0, -1.0) ;
+        LeftFrontPower   = Range.clip(strafe, -1.0, 1.0) ;
+
+        RightBackPower = Range.clip(turn, -1.0, 1.0);
+        RightFrontPower = Range.clip(turn,-1.0, 1.0);
+        LeftBackPower = Range.clip(turn,1.0,-1.0);
+        LeftFrontPower = Range.clip(turn,1.0,-1.0);
+
 
 
         // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
-
+        RightBack.setPower(RightBackPower);
+        RightFront.setPower(LeftBackPower);
+        LeftBack.setPower(RightFrontPower);
+        LeftFront.setPower(LeftFrontPower);
     }
 
     /*
